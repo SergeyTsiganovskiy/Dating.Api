a6e6c87cea948bf3ed526e31272f05399c448ba5
SerhiiTsyhanivskyi <tsyganovskiyss@gmail.com>
2019-10-26 19:33:17 +0300

    added authentication with a token

---
+++ Controllers/Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+namespace Dating.API.Controllers.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }    
+        public string Password { get; set; }
+    }
+}
