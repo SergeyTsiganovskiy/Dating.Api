d5de01bc41fdfcd59a56e0c9865e1d6e1505a5d6
Serhii Tsyhanivskyi <Serhii_Tsyhanivskyi@epam.com>
2019-11-12 13:00:14 +0200

    added controller for handling updating user

---
+++ Controllers/Dtos/UserForUpdateDto.cs
@@ -0,0 +1,11 @@
+namespace Dating.API.Controllers.Dtos
+{
+    public class UserForUpdateDto
+    {
+        public string Introduction { get; set; }
+        public string LooingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
