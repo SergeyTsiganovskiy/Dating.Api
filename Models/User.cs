767735eacc20096d03d3cb376d2e6d566352a8d7
Serhii Tsyhanivskyi <Serhii_Tsyhanivskyi@epam.com>
2019-11-19 11:59:27 +0200

    added like functionality

--- Models/User.cs
+++ Models/User.cs
@@ -21,6 +21,9 @@ namespace Dating.API.Models
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public ICollection<Like> Liker { get; set; }
+        public ICollection<Like> Likee { get; set; }
+
         public User()
         {
             Photos = new Collection<Photo>();
